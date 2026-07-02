e0adf741af53c5c3c005221aafbb3c6e2ebb7d56
moretti123 <tuxigoww@bk.ru>
2026-07-02 20:14:39 +0300

    Make admin

--- make_me_admin.py
+++ make_me_admin.py
@@ -3,7 +3,7 @@ from models import User
 
 with app.app_context():
     # Найди себя по email
-    user = User.query.filter_by(email='ТВОЙ_EMAIL').first()
+    user = User.query.filter_by(email='tuxigoww@bk.ru').first()
     if user:
         user.role = 'admin'
         db.session.commit()
